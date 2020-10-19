11487f76061fde8bbcab0a449aca00012dd86457
Jaguaraquem Adler Reinaldo <jaguar.adler@protonmail.com>
2020-10-19 15:56:33 -0300

    Added hashCode and equals implementation;

--- src/main/java/net/intelie/challenges/Event.java
+++ src/main/java/net/intelie/challenges/Event.java
@@ -1,9 +1,12 @@
 package net.intelie.challenges;
 
+import java.util.Objects;
+
 /**
  * This is just an event stub, feel free to expand it if needed.
  */
 public class Event {
+
     private final String type;
     private final long timestamp;
 
@@ -18,5 +21,28 @@ public class Event {
 
     public long timestamp() {
         return timestamp;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.type);
+        hash = 17 * hash + (int) (this.timestamp ^ (this.timestamp >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final Event other = (Event) obj;
+        return !Objects.equals(this.type, other.type)
+                && this.timestamp != other.timestamp;
     }
 }
